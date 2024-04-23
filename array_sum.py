79aadf3354f0cd5421a57b7b290a100ea4eb7011
jadonblaise <chinedublaise@yahoomail.com>
2024-04-23 12:07:52 +0200

    learning_arrays

---
+++ array_sum.py
@@ -0,0 +1,64 @@
+"""
+Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+
+Constraints
+
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+Explanation
+
+We print the sum of the array's elements: ."""
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    # Read the size of the array from input 
+    ar_count = int(input().strip())
+
+    # Read the elements of the array from the input
+    ar = list(map(int, input().rstrip().split()))
+
+    # Call the function with the array as argument and store the result
+    result = simpleArraySum(ar)
+
+    print(result)
